705a046c22988a026f809b9b3d6a0ccba3b75e55
andres-xicara <andres.xicara@gmail.com>
2025-06-04 21:16:54 -0600

    noSIRVE

--- main.py.py
+++ main.py.py
@@ -56,7 +56,7 @@ Motor_LB_Dir.direction = digitalio.Direction.OUTPUT
 Motor_LB_Dir.value = False
 
 class Motor():
-    def __init__(self):
+    def _init_(self):
         pass
 
 
@@ -106,7 +106,7 @@ class Motor():
         Motor_RB_Dir.value = False
         Motor_RB_PWM.duty_cycle = 0
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     motor = Motor()
     try:
         motor.motor_stop()
@@ -118,10 +118,10 @@ if __name__ == '__main__':
 
         # SECUENCIA 1 - Llevar carrito a estación 2
         print("Avanza hasta llegar al carrito...")
-        motor.motor_left_back(1, LB_forward, 70)
-        motor.motor_right_front(1, RF_forward, 30)
-        motor.motor_right_back(1, RB_forward, 30)
-        motor.motor_left_front(1, LF_forward, 70)
+        motor.motor_left_back(1, LB_forward, 1) #front RIGHT
+        motor.motor_right_front(1, RF_forward, 70) #back LEFT
+        motor.motor_right_back(1, RB_forward, 1) #back RIGHT
+        motor.motor_left_front(1, LF_forward, 1) #front LEFT
         time.sleep(5)  # Simula llegar al carrito
 
         print("Frena...")
@@ -165,4 +165,4 @@ if __name__ == '__main__':
         motor.motor_stop()
 
     except KeyboardInterrupt:
-        motor.motor_stop() 
+        motor.motor_stop()
